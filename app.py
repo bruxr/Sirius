db93bb124420d7d289c03cb6f52749467430f08a
Brux Romuar <brux.romuar@gmail.com>
2021-01-18 07:22:16 +0800

    Add response

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-    return 'Hello World!'
-
-@app.route('/ping')
-def ping():
-    return '', 204;
+    return 'hi'
