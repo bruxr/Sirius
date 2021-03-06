e44a4b1d570937052091b77b379f95738e1fefc3
Brux Romuar <brux.romuar@gmail.com>
2021-03-07 02:15:24 +0800

    Fix auth

--- sirius/auth.py
+++ sirius/auth.py
@@ -10,12 +10,11 @@ def login():
     password = request.form['password']
   
     if not valid_login(username, password):
-      print('invalid lol', flush=True)
       flash('Invalid username and/or password.')
-  
-    session.clear()
-    session['couch_username'] = username
-    session['couch_password'] = password
-    return redirect(url_for('admin.index'))
+    else:
+      session.clear()
+      session['couch_username'] = username
+      session['couch_password'] = password
+      return redirect(url_for('admin.index'))
   
   return render_template('auth/login.html')
