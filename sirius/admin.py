d19eb0e47de1c84601820546b1d101ee2fbf52d0
Brux Romuar <brux.romuar@gmail.com>
2021-03-07 15:11:17 +0800

    Properly check authentication

--- sirius/admin.py
+++ sirius/admin.py
@@ -2,10 +2,10 @@ from flask import Blueprint, request, session, redirect, url_for, render_templat
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
-@bp.before_app_request
+@bp.before_request
 def check_logged_in_user():
   if session.get('couch_username') is None or session.get('couch_password') is None:
-    redirect(url_for('auth.login'))
+    return redirect(url_for('auth.login'))
 
 @bp.route('/')
 def index():
