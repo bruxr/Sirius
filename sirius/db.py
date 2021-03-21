4dc641f1489160c2e87b3931b44cf1507bfe3b13
Brux Romuar <brux.romuar@gmail.com>
2021-03-21 12:04:58 +0800

    Use settings for db credentials

--- sirius/db.py
+++ sirius/db.py
@@ -1,26 +1,24 @@
 import couchdb
 from flask import session
 
-def valid_login(username, password):
-  """Validates credentials and returns True if they are valid, False otherwise."""
-  couch = couchdb.Server(f'http://{username}:{password}@couchdb:5984')
-  try:
-    couch.version()
-  except:
-    return False
-  return True
+from .settings import COUCHDB_USER, COUCHDB_PASS, COUCHDB_HOST
 
-def db_connect(username, password):
-  couch = couchdb.Server(f'http://{username}:{password}@couchdb:5984')
-  return couch['polaris']
+
+def valid_login(username, password):
+    """Validates credentials and returns True if they are valid,
+       False otherwise.
+    """
+    couch = couchdb.Server(f'http://{username}:{password}@couchdb:5984')
+    try:
+        couch.version()
+    except:
+        return False
+    return True
+
 
 def get_db():
-  """Retrieves a database connection using the user's stored credentials"""
-  username = session.get('couch_username')
-  password = session.get('couch_password')
-  
-  if username is None or password is None:
-    return None
-    
-  return db_connect(username, password)
-  
+    """Returns a database connection"""
+    couch = couchdb.Server(
+        f"http://{COUCHDB_USER}:{COUCHDB_PASS}@{COUCHDB_HOST}"
+    )
+    return couch['polaris']
---
+++ sirius/settings.py.tpl
@@ -0,0 +1,3 @@
+COUCHDB_USER = "admin"
+COUCHDB_PASS = "secret"
+COUCHDB_HOST = "couchdb:5984"
